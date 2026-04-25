f22ad39c7809d73e366156d9edcd63c649ad431a
Yansa27 <juliansa837@gmail.com>
2026-04-26 01:24:19 +0700

    fix : merubah struktur file dan folder

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Auth\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+   
+}
