8d1a57b0093bc21660cbdcada5c64a5e7cf19e39
Sidiq Fatuh <kmzbengbeng14@gmail.com>
2026-05-12 20:31:10 +0700

    chore: update database users, menambahkan bio,alamat,no_hp

---
+++ database/migrations/2026_05_12_131356_add_profile_fields_to_users_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->text('bio')->nullable()->after('email');
+            $table->string('alamat')->nullable()->after('bio');
+            $table->string('no_hp', 15)->nullable()->after('alamat');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn(['bio', 'alamat', 'no_hp']);
+        });
+    }
+};
